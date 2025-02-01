a871dc8097ce5c3991a1b4a6f446821c50c77962
Hanakorov <kirillishka2007@gmail.com>
2025-02-01 13:02:12 +0500

    upt

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@ use teloxide::prelude::*;
 
 #[tokio::main]
 async fn main() {
-    // Создаем бота, который получает токен из переменной окружения TELOXIDE_TOKEN
     let bot = Bot::from_env();
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        // Проверяем, есть ли текст в сообщении и является ли он командой "/dice"
         if let Some(text) = msg.text() {
             if text == "/dice" {
                 bot.send_dice(msg.chat.id).await?;
